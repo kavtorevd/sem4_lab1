adf955eccdf461c53f2c2099ab54fd0d918567fa
kavtorevd <dimakavtorev@gmail.com>
2024-03-25 23:18:05 +0300

    added polinom

--- include/polinom/TMonom.h
+++ include/polinom/TMonom.h
@@ -1,3 +1,58 @@
-#pragma once
+﻿#pragma once
+struct TMonom
+{
+	double coef; // êîýôôèöèåíò ìîíîìà
+	int index; // èíäåêñ (ñâåðòêà ñòåïåíåé)
 
-#include "Stack/stack.h"
+
+	TMonom() {
+		coef = 0;
+		index = 0;
+	}
+
+	TMonom(double co, int degX, int degY, int degZ) {
+		index = degX * 100 + degY * 10 + degZ;
+		coef = co;
+	}
+
+	void SetCoef(double cval) {
+		coef = cval;
+	}
+	double GetCoef(void) {
+		return coef;
+	}
+	void SetIndex(int ival) {
+		index = ival;
+	}
+	int GetIndex(void) { return index; }
+
+
+	bool operator==(const TMonom& other) {
+		if ((other.coef == this->coef) && (other.index == this->index)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool operator>(const TMonom& other) {
+		if ((other.coef > this->coef) && (other.index > this->index)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool operator<(const TMonom& other) {
+		if ((other.coef < this->coef) && (other.index < this->index)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+};
